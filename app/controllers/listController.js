cd98c98e81bb2188b2cc2a5548e17b1c448daec6
jMoirenc <julien.moirenc@oclock.io>
2022-10-24 15:25:00 +0200

    include des cards et des tags dans /lists en GET

--- app/controllers/listController.js
+++ app/controllers/listController.js
@@ -1,9 +1,23 @@
-const { List } = require ('../models/index');
+const { List, Card, Tag } = require ('../models/index');
 
 const listController = {
   getAllLists: async (req, res) => {
     try{
-      const lists = await List.findAll({order: ['position']});
+      const lists = await List.findAll({
+        include: {
+          model: Card,
+          as: 'cards',
+          include: {
+            model: Tag,
+            as: 'tags',
+          }
+        },
+        order: [
+          ['position', 'ASC'],
+          ['cards', 'position', 'ASC'],
+        ]
+      });
+
       res.json(lists);
     }catch(error){
       console.trace(error);
